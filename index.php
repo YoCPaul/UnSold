ceaa7b77337ab7d498d34d9223ba7cfde3e0d556
YoCPaul <67915507+YoCPaul@users.noreply.github.com>
2024-01-11 10:34:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php echo 'Hello World !'; ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <p>This is an example of a simple HTML page with one paragraph.</p>
+    </body>
+</html>
